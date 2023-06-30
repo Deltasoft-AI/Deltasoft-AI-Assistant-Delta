6fc601090b71de095c47accd072da4b40db182a7
JSman225 <84781179+JSman225@users.noreply.github.com>
2023-06-30 15:29:02 -0400

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -1,16 +1,15 @@
 // Require the slash command builder
 const { SlashCommandBuilder } = require('@discordjs/builders');
-const db = require('replitdb-client');
-
-const dbClient = new db('https://kv.replit.com/v0/eyJhbGciOiJIUzUxMiIsImlzcyI6ImNvbm1hbiIsImtpZCI6InByb2Q6MSIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJjb25tYW4iLCJleHAiOjE2ODUyNDc1ODQsImlhdCI6MTY4NTEzNTk4NCwiZGF0YWJhc2VfaWQiOiI2ODZmMjAzYS1mMzZhLTRlZTUtYjQwNy0yZjgyZTRhYmY4MzQiLCJ1c2VyIjoiRmlnaThDYWtlIiwic2x1ZyI6IkpzbWFuLUJvdCJ9.KxNYI4ewByZ2LSSuHQ2uvcu6vIHxSTkzuqoZ36G40PaBLC-zDeyCpiPcQBGr6AvpaPt5PDUwsvHkl2XzoCxACg');
+const Database = require("@replit/database");
+const db = new Database()
 // Export as a module for other files to require()
 module.exports = {
   data: new SlashCommandBuilder() // command details
     .setName('clear')
     .setDescription('Clear the chat history'),
   async execute(interaction) { // command functions
-    dbClient.delete(interaction.user.id);
-    const exampleEmbed = {
+    db.delete(interaction.user.id);
+  const exampleEmbed = {
       color: 12114155,
       author: {
         name: 'Deltasoft AI',
@@ -26,7 +25,6 @@ module.exports = {
       },
       timestamp: new Date().toISOString(),
     };
-  
-    return interaction.reply({ embeds: [exampleEmbed] });
+    return interaction.reply({ embeds: [exampleEmbed], ephemeral: true });
   },
 };
